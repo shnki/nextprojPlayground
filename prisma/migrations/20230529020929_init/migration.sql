b469eb8d4bf877ab63ae56516e32dc74fc3f5685
Mohamed Yasser <51481220+shnki@users.noreply.github.com>
2023-08-07 08:58:13 +0300

    replacing sqlite with postgress

--- prisma/migrations/20230529020929_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "avaliable" BOOLEAN NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
